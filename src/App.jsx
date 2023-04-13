140af7704335b8a9c7145aff82048ddf17d06fbf
Max Cheever <cheevermax@gmail.com>
2023-04-13 12:12:41 -0400

    collective box populating

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import "./App.css";
 import Entry from "./Entry/Entry.jsx";
 
 function App() {
-	const [hex1, setHex1] = useState("ffffff");
-	const [hex2, setHex2] = useState("ffffff");
+	const [hex1, setHex1] = useState("FFFFFF");
+	const [hex2, setHex2] = useState("FFFFFF");
 
 	return (
 		<>
@@ -17,7 +17,7 @@ function App() {
 			<div className="bottom">
 				<ColorBox hex={hex1} size={"40"} />
 				<p className="bottomColor">#{hex1}</p>
-				<Entry update={setHex1} />
+				<Entry update1={setHex1} update2={setHex2} />
 			</div>
 		</>
 	);
--- src/Entry/Entry.jsx
+++ src/Entry/Entry.jsx
@@ -2,9 +2,10 @@ import { useState, useRef } from "react";
 import "./Entry.css";
 import { Keccak } from "sha3";
 
-function Entry({ update }) {
+function Entry({ update1, update2 }) {
 	const [entry, setEntry] = useState("");
 	const ref = useRef(null);
+	const [entrys, setEntrys] = useState([]);
 
 	// hashes the name from the entry box
 	function hashing(name) {
@@ -15,18 +16,57 @@ function Entry({ update }) {
 		return hashed;
 	}
 
+	// converts from hexadecimal to binary or vice versa
+	function BaseConverter(num, indicator) {
+		var final =
+			indicator === 1
+				? parseInt(num, 16).toString(2)
+				: parseInt(num, 2).toString(16);
+
+		// 0 extends to 24 bits
+		var extend = indicator === 1 ? 24 - final.length : 6 - final.length;
+		var bit = "0";
+		final = bit.repeat(extend) + final;
+
+		return final;
+	}
+
+	function collective(collection) {
+		var collectionB = [];
+		for (let k = 0; k < collection.length; k++) {
+			collectionB.push(BaseConverter(collection[k], 1));
+		}
+		var finalHash = ""; // will be 24 bit binary
+
+		// adds most common bit at each index to finalHash
+		for (var i = 0; i < 24; i++) {
+			var check = 0;
+			for (let j = 0; j < collectionB.length; j++) {
+				collectionB[j][i] === "1" ? (check += 1) : (check -= 1);
+			}
+			check > 0 ? (finalHash += "1") : (finalHash += "0");
+		}
+
+		// convert final to hex
+		finalHash = BaseConverter(finalHash, 0);
+
+		return finalHash;
+	}
+
 	const onNameChange = (event) => {
 		setEntry(event.target.value);
 	};
 
 	const onButtonClick = () => {
 		var hash = hashing(entry);
-		update(hash);
+		setEntrys([hash, ...entrys]);
+
+		update1(hash); // setting big colorbox to 6 digit hash
+		update2(collective(entrys)); // setting small colorbox to 6 digit collective hash
+
 		setEntry("");
 		ref.current.value = "";
 	};
-
-	let last10 = {};
 
 	return (
 		<>
