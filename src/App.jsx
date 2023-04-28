8cb927dc162c77549e8a9d20488cacbaf7c4feee
Max Cheever <cheevermax@gmail.com>
2023-04-28 15:56:48 -0400

    added repo link

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,23 @@ function App() {
 				<p className="bottomColor">#{hex1}</p>
 				<Entry update1={setHex1} update2={setHex2} />
 			</div>
+			<div className="link-div">
+				<a
+					href="https://github.com/maxcheever/yourfavoritecolor"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<i className="fa fa-github"></i>
+				</a>
+				<a
+					href="https://github.com/maxcheever/yourfavoritecolor"
+					className="link"
+					target="_blank"
+					rel="noreferrer"
+				>
+					learn more about this piece
+				</a>
+			</div>
 		</>
 	);
 }
